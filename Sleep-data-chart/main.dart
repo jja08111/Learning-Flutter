139d7ef09168f2ff80c5244fbbb9b05aff242190
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2020-11-11 19:09:04 +0900

    add new sleep data chart

--- Sleep-data-chart/main.dart
+++ Sleep-data-chart/main.dart
@@ -1,40 +1,38 @@
-import 'sleep_data_charts.dart';
+import 'package:app_for_blog_posting/sleep_data_chart/sleep_data_chart.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   /// 수면 그래프 테스트 데이터
   ///
-  /// 기상 시간
-  final List<double> dataWakeUpTime = [8, 10, 11, 8];
   /// 수면량
-  final List<double> dataAmount = [8, 9, 7, 5];
+  final List<double> dataAmount = [5.77, 4.9, 7.75, 4, 4, 4.65, 9.1];
+  /// 기상 시간
+  final List<double> dataWakeUpTime = [6.3, 12, 8.32, 10.36, 5.4, 5, 6.4];
   /// 날짜
   final List<String> labels = [
-    "10/1",
-    "10/2",
-    "10/3",
-    "10/4",
+    "Mon",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Wed",
+    "Thu",
+    "Fri",
   ];
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      color: Color(0xff121212),
       home: Center(
-        child: Container(
-          child: CustomPaint(
-            size: Size(300, 300),
-            foregroundPainter: SleepDataChart(
-              dataWakeUpTime: dataWakeUpTime,
-              dataAmount: dataAmount,
-              labels: labels,
-            ),
-          ),
+        child: SleepDataChart(
+          barColor: Colors.lightBlueAccent,
+          dataWakeUpTime: dataWakeUpTime,
+          dataAmount: dataAmount,
+          labels: labels,
         ),
       ),
     );
